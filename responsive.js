84b59209bdb34f5d149284ad38646fd9550d4f4c
Engin BEYAZGÜL <engin.beyazgul@yildiz.com.tr>
2025-12-08 15:43:18 +0300

    Düzenlemeler yapıldı.

--- responsive.js
+++ responsive.js
@@ -9,4 +9,20 @@ $(document).ready(function(){
         $('.menu').toggle();
         $('.menu2').toggle();
     });
+    $(document).click(function(e){
+        if ($('.mobile-menu').hasClass('active')){
+            if ($(e.target).closest('.mobile-menu, .menu, .menu2').length === 0){
+                $('.mobile-menu').removeClass('active');
+                $('.menu').show();
+                $('.menu2').hide();
+            }
+        }
+    });
+    $('.mobile-menu').on('click', 'a', function(){
+        if ($('.mobile-menu').hasClass('active')){
+            $('.mobile-menu').removeClass('active');
+            $('.menu').show();
+            $('.menu2').hide();
+        }
+    });
 });
