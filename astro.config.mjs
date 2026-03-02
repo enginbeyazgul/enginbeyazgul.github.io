fb689e13170000172de3c6330e422966523325f5
Engin Beyazgül <beyazgulengin@gmail.com>
2026-03-03 00:08:27 +0300

    feat(cms): remove sanity and switch to decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sanity from '@sanity/astro';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://enginbeyazgul.dev',
-    integrations: [
-        sanity({
-            projectId: 'c4doe6ti',
-            dataset: 'production',
-            useCdn: true,
-            apiVersion: '2024-03-02',
-        }),
-    ],
 });
